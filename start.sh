4c2ff5bc02f41e5a70597934b1daa4d61be6391b
Rishi Agrawal <rishiagrawal2609@gmail.com>
2022-03-29 03:53:43 +0000

    start.sh for ubuntu based system up and running

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update
+sudo apt install curl
+sudo apt install python3
+sudo apt install terraform
+terraform -v
